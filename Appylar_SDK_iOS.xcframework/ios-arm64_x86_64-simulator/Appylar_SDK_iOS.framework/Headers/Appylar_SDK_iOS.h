5f895e3e1ffc318b57d68b0904e0e1c09f60c150
Aziz Saifee <azizsaifee123@gmail.com>
2023-03-05 11:48:37 +0530

    new file added

---
+++ Appylar_SDK_iOS.xcframework/ios-arm64_x86_64-simulator/Appylar_SDK_iOS.framework/Headers/Appylar_SDK_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Appylar_SDK_iOS.h
+//  Appylar SDK iOS
+//
+//  Created by 5Exceptions_Mac1 on 16/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Appylar_SDK_iOS.
+FOUNDATION_EXPORT double Appylar_SDK_iOSVersionNumber;
+
+//! Project version string for Appylar_SDK_iOS.
+FOUNDATION_EXPORT const unsigned char Appylar_SDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Appylar_SDK_iOS/PublicHeader.h>
+
+
